c9235fc756bef027c9ade28d50e91d620321e70f
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-03 15:54:06 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  root 'posts#index'
 
-  get 'posts/new'
-
-  get 'posts/show'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/delete'
+  get 'posts/new' => 'posts#new', as: :new_post
+  get 'posts/:id' => 'posts#show', as: :post
+  post 'posts' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit', as: :edit_post
+  patch 'posts/:id' => 'posts#update'
+  delete 'posts/:id' => 'posts#delete'
+  post 'posts/:id/comments' => 'posts#create_comment', as: :comments
+  post 'posts/:id/votes' => 'posts#update_votes', as: :votes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
