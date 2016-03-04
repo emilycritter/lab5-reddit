a8e7a75c176e0ac30fe5402455d8fed6df14cd1e
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-04 00:19:51 -0600

    Comments now functional

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  validates :title, :vote_count, :added_by, presence: true
+  validates :title, :vote_count, presence: true
 
   def scrape_posts
     require 'rubygems'
