ba5cf0dc9d1c89112dd4a3fa2606d239cc29dfd9
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-03 19:32:42 -0600

    Remove duplicates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user_id
 
+  validates :user_id, presence: true, default: 1
+
   def scrape_posts
     require 'rubygems'
     require 'mechanize'
@@ -19,6 +21,8 @@ class Post < ActiveRecord::Base
         post.vote_count = reddit_post.search('.unvoted .unvoted').text.strip
         user = User.new
         user.username = reddit_post.search('.tagline .author').text.strip
+        user.save
+        puts user.id
         post.user_id = user.id
         post.link_url = reddit_post.search('.title .title').to_s
         post.link_url = post.link_url.split('" tabindex=')[0].to_s
@@ -29,7 +33,11 @@ class Post < ActiveRecord::Base
         post.save
       end
     end
-    Post.group_by{|x| [x.link_url]}.flat_map{|_,x| x.drop(1)}.destroy_all
+
+    # Remove duplicates
+    Post.all.each do |post|
+      Post.order("id desc").where(link_url: post.link_url).all.drop(1).each { |r| r.delete }
+    end
     puts Post.count
   end
 
